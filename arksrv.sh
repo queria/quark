2e835a0a40b8c939d13da3f2ec087568def22bbb
Pavel Sedlák <psedlak@redhat.com>
2017-01-12 17:23:25 +0100

    arksrv.sh: move QUDIR/CFG/... vars to top
    This moves QUDIR (this repo) based file paths
    to top of arksrv.sh, as variables,
    so they can be reused in 'install' help.
    
    Adds QUPRIV and QUADMCFG variables,
    representing 'private' and 'AllowedCheaterSteamIDs.txt'.

--- arksrv.sh
+++ arksrv.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -ex
 
+QUDIR=$(dirname "$(readlink -f "$0")")
+QUCFG="${QUDIR}/DefaultGameUserSettings.ini"
+QUPRIV="${QUDIR}/private"
+QUADMCFG="${QUDIR}/AllowedCheaterSteamIDs.txt"
+
 if [[ "$1" == "install" ]]; then
 	# $ useradd steam
 	# $ su steam
@@ -17,27 +22,24 @@ if [[ "$1" == "install" ]]; then
 		$HOME/Steam/steamcmd.sh +login anonymous +force_install_dir $HOME/arksrv +app_update 376030 +quit
 	fi
 
-	[[ -f "$QUDIR/private" ]] || echo "Create $QUDIR/private (see private.example) before starting server"
-	[[ -f "$QUDIR/AllowedCheaterSteamIDs.txt" ]] || echo "Possibly create also AllowedCheaterSteamIDs.txt (admins steamid64 per line)"
+	[[ -f "$QUPRIV" ]] || echo "Create $QUDIR/private (see private.example) before starting server"
+	[[ -f "$QUADMCFG" ]] || echo "Possibly create also AllowedCheaterSteamIDs.txt (admins steamid64 per line)"
 	exit
 fi
 
-QUDIR=$(dirname "$(readlink -f "$0")")
-QUCFG="${QUDIR}/DefaultGameUserSettings.ini"
 CFG="$HOME/arksrv/ShooterGame/Config/DefaultGameUserSettings.ini"
 [[ ! -h "$CFG" ]] && rm -f "$CFG"
 [[ -f "$CFG" ]] || ln -snf "$QUCFG" "$CFG"
 
-QUCFG="${QUDIR}/DefaultGameUserSettings.ini"
 CFG="$HOME/arksrv/ShooterGame/Saved/AllowedCheaterSteamIDs.txt"
-if [[ -f "${QUCFG}" ]]; then
+if [[ -f "${QUADMCFG}" ]]; then
 	[[ -d "$(dirname "$CFG")" ]] || mkdir -p "$(dirname "$CFG")"
 	[[ -h "$CFG" ]] || rm -f "$CFG"
-	[[ -f "$CFG" ]] || ln -snf "$QUCFG" "$CFG"
+	[[ -f "$CFG" ]] || ln -snf "$QUADMCFG" "$CFG"
 fi
 
 OPTS=""
-source "$QUDIR/private"
+source "$QUPRIV"
 OPTS="${OPTS} -server -log -nomansky -NoBattlEye"
 
 cd $HOME/arksrv/ShooterGame/Binaries/Linux/
